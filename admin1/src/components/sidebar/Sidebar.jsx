fa7d8a95898ca3ff92b1f8eb346b1dc7511ab648
Amba34 <ambadasjoshi34@gmail.com>
2025-01-20 15:25:53 +0530

    Update Sidebar.jsx

--- admin1/src/components/sidebar/Sidebar.jsx
+++ admin1/src/components/sidebar/Sidebar.jsx
@@ -32,7 +32,7 @@ const Sidebar = () => {
     <div className="sidebar">
       <div className="top">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">lamadmin</span>
+          <span className="logo">admin</span>
         </Link>
       </div>
       <hr />
